815fa0e3b4a954dad9410f68993571a5cdcfaf5f
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-08-09 23:04:24 +0300

    add list people

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -1,8 +1,39 @@
+import { useState, useEffect } from 'react';
+import { getApiResource } from '../../utils/network'
+import { API_PEOPLE } from '../../constans/api'
+
 import styles from "./PeoplePage.module.css";
 
 const PeoplePage = () => {
+
+  const [people, setPiople] = useState(null);
+
+
+  const getResource = async (url) => {
+    const res = await getApiResource(url);
+    const peopleList = res.results.map(({ name, url }) => {
+      return {
+        name,
+        url
+      }
+    })
+    setPiople(peopleList);
+  }
+  useEffect(() => {
+    getResource(API_PEOPLE);
+  }, []);
   return (
-    <>PeoplePage</>
+    <>
+      {people && (
+        <ul>
+          {people.map(({ name, url }) =>
+            <li key={name}>
+              {name}
+            </li>
+          )}
+        </ul>
+      )}
+    </>
   );
 };
 

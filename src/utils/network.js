562f61d1904f7285dbe29f426cfcdfea9398d13c
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-08-09 08:51:22 +0300

    add async

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,11 +1,32 @@
 const SWAPI_ROOT = "https://swapi.dev/api/";
 const SWAPI_PEOPLE = "people";
 
-export const getApiResource = (url) => {
-  fetch(url)
-    .then((res) => res.json())
-    .then((body) => console.log(body))
-    .catch(error=> console.log(error.message))
+export const getApiResource = async (url) => {
+  try {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      console.error("Could not fetch.", res.status);
+      return false;
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error("Could not fetch.", error.message);
+    return false;
+  }
 };
 
-getApiResource(SWAPI_ROOT + SWAPI_PEOPLE)
+// getApiResource(SWAPI_ROOT + SWAPI_PEOPLE)
+//     .then((body) => console.log(body));
+
+    (async()=> {
+
+        const body = await getApiResource(SWAPI_ROOT + SWAPI_PEOPLE);
+        console.log(body)
+    })()
+
+
+
+// const body = getApiResource(SWAPI_ROOT + SWAPI_PEOPLE);
+// console.log(body)

45ad621c6fe88050391155713dfeb1f0b0952d35
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-08-10 11:47:21 +0300

    add error url

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -11,27 +11,45 @@ import styles from "./PeoplePage.module.css";
 const PeoplePage = () => {
 
   const [people, setPeople] = useState(null);
+  const [errorApi, setErrorApi] = useState(false);
 
   const getResource = async (url) => {
-    const res = await getApiResource(url);
-    const peopleList = res.results.map(({ name, url }) => {
-      const id = getPeopleId(url);
-      const img = getPeopleImage(id);
+    const res = await getApiResource(1 + url);
 
-      return {
-        id,
-        name,
-        img
-      }
-    })
-    setPeople(peopleList);
+    if (res) {
+      const peopleList = res.results.map(({ name, url }) => {
+        const id = getPeopleId(url);
+        const img = getPeopleImage(id);
+
+        return {
+          id,
+          name,
+          img
+        }
+      })
+      setPeople(peopleList);
+      setErrorApi(false);
+    } else {
+      setErrorApi(true);
+    }
+
+
   }
   useEffect(() => {
     getResource(API_PEOPLE);
   }, []);
   return (
     <>
-      {people && <PeopleList people={people} />}
+      {errorApi
+        ? <h2 style={{ padding: "10px 20px", textAlign: "center", color: 'red' }}>Error</h2>
+        :
+        (
+          <>
+            <h1>Navigation</h1>
+            {people && <PeopleList people={people} />}
+          </>
+        )
+      }
     </>
   );
 };
